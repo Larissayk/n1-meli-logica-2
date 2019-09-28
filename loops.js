a375d5022004c885c2e9dc1c2a9f3e92fce375fc
Larissayk <lari.yukie@gmail.com>
2019-09-27 22:11:45 -0300

    desafios6

---
+++ loops.js
@@ -0,0 +1,21 @@
+// Loop com while
+
+let contador = 0;
+while(contador < 6) {
+    console.log('Olá');
+    contador++; //contador = contador + 1
+}
+
+// Loop com Do...While
+
+do{
+    console.log('Olá');
+    contador++;
+}while(contador <6);
+
+
+//Loop com For
+
+for(let contador=0; contador<6; contador++){
+    console.log('olá');
+}
